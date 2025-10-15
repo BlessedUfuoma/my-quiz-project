bbdaae9103d14811663de780da4006d36eab04e2
ufuoma blessed <ufuomablessed17@gmail.com>
2025-10-16 00:03:54 +0100

    add responsiveness to signup button

--- my-quiz-app/src/pages/Login.jsx
+++ my-quiz-app/src/pages/Login.jsx
@@ -1,14 +1,32 @@
 import React from 'react'
+import { useNavigate } from "react-router-dom";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth } from "../firebase";
 import logo from "../assets/quizlogo.svg";
 
 
 const Login = () => {
+  const navigate = useNavigate();
+  const handleGoogleSignIn = async () =>
+  {
+    try {
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+
+      console.log("User info:", user);
+      alert('Welcome & {user. displayName}!');
+      navigate ("/dashboard");
+    } catch (error) {
+      console.error("Google signin error:", error);
+    }
+  };
+
   return (
   <div class="h-screen w-full bg-gradient-to-b from-[#3B82F6] to-[#999999]">
       <div class="flex justify-center items-center md:shrink"> 
         <img
+        style={{marginTop: 20 + 'px'}}
         class="h-18 " 
          src={logo} alt="" />
       </div>
@@ -17,7 +35,9 @@ const Login = () => {
         <h2 class="text-center font-[Open_Sans] font-bold text-white">"Where curiousity meets knowledge"</h2>
    
         <div class="text-center">
-        <button class="bg-gradient-to-br from-[#434799] to-[#747AF5] text-white px-8 py-1 rounded-4xl mt-80 mb-20 font-[Open_Sans] font-bold"  >Signup with Google</button>
+        <button 
+        onClick={handleGoogleSignIn}
+        class="bg-gradient-to-br from-[#434799] to-[#747AF5] text-white px-8 py-1 rounded-4xl mt-80 mb-20 font-[Open_Sans] font-bold"  >Signup with Google</button>
         <p class="text-white font-[Open_Sans] font-bold">Already have an account? Login</p>
     </div>
   </div>
